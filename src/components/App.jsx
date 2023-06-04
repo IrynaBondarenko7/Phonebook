6f9ef53b34b3daf53dabd4715c3dafeca87cb04f
Iryna Bondarenko <ira.bondarenko@ukr.net>
2023-06-04 14:56:46 +0100

    added start files

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,43 @@
+import { useSelector } from 'react-redux';
+import { ContactList } from './Contacts/ContactList';
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { StyledLayout } from './Layout/Layout.styled';
+
+import {
+  StyledPhonebookWrap,
+  StyledContactsTitle,
+  StyledTitle,
+  StyledTitleWrap,
+} from './App.styled';
+import { FaBook } from 'react-icons/fa';
+import { IoMdContacts } from 'react-icons/io';
+import { selectError, selectIsLoading } from 'redux/selectors';
+import { Loader } from './Loader/Loader';
+
 export const App = () => {
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <StyledLayout>
+      <StyledPhonebookWrap>
+        <StyledTitleWrap>
+          <StyledTitle>Phonebook</StyledTitle>
+          <FaBook />
+        </StyledTitleWrap>
+
+        <ContactForm />
+        <StyledTitleWrap>
+          <StyledContactsTitle>Contacts</StyledContactsTitle>
+          <IoMdContacts />
+        </StyledTitleWrap>
+
+        <Filter />
+        {isLoading && !error && <Loader />}
+        {error && <p>Something went wrong, try again!</p>}
+        <ContactList />
+      </StyledPhonebookWrap>
+    </StyledLayout>
   );
 };
